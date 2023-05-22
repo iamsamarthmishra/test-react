9a54894b2332adb26b4bfcf5fe7e365e2fdeca10
Samarth Mishra <91658673+Samarth277@users.noreply.github.com>
2023-05-22 11:01:33 +0530

    Update portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -23,7 +23,9 @@ const greeting = {
   logo_name: "Samarthmishra",
   nickname: "stackdevsamarth",
   subTitle:
-    "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
+    "I'm a passionate Full Stack Developer and Penetration Tester with expertise in web development, ethical hacking, and data structures. Currently pursuing a BBA, I'm proficient in HTML, JS, PHP, MongoDB, Linux, and Google Cloud Terminal. I've contributed to Internet Archive.org and the Tor Project through Google Summer of Code 2023. I hold the CEH certification from IIT Kharagpur and completed courses in JavaScript, Python, reverse engineering, and ethical hacking. CISSP certified. I enjoy reading and cooking, and aspire to teach coding and cybersecurity. Let's collaborate on exciting projects! Reach out to me!",
+
+
   resumeLink:
     "https://drive.google.com/file/d/16ighlMjEdZ7ZrN_YLNhkXnL-zr4XN2hl/view",
   portfolio_repository: "https://github.com/stackdevsamarth",
